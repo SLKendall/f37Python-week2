94be379ad6b2a6bcb8b2e32ccc05122f2f2b1ec7
Spencer Kendall <spencerlkendall@gmail.com>
2023-06-07 20:15:01 -0500

    Initial Commit

---
+++ starter/server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for, redirect
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    cupcakes = get_cupcakes("cupcakes.csv")
+    order = get_cupcakes("orders.csv")
+    return render_template("index.html", cupcakes=cupcakes, order = order)
+
+
+@app.route("/cupcakes")
+def all_cupcakes():
+    return render_template("cupcakes.html")
+
+@app.route("/individual_cupcake/<name>")
+def individual_cupcake():
+    return render_template("individual-cupcake.html")
+    
+@app.route("/order")
+def order():
+    cupcakes = get_cupcakes("orders.csv")
+    total = 0
+    for cupcake in cupcakes:
+        total += float(cupcake["price"])
+    return render_template("/order.html", cupcakes=cupcakes, total=total)
+
+
+@app.route("/add-cupcake/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("cupcakes.csv", name)
+
+    if cupcake: 
+        add_cupcake_dictionary("orders.csv", cupcake)
+        return redirect(url_for("home"))
+    else:
+        return "Cupcake Not Found"
+
+if __name__ == "__main__":
+    app.env = "development"
+    app.run(debug = True, port = 8000, host = "localhost")
